24176530ac4663e1b24c2d6bd513bf538030e768
wangsuwen <1002853070@qq.com>
2024-03-13 12:01:13 +0800

    添加：虚拟列表

---
+++ components/songli-list/songli-list.vue
@@ -0,0 +1,115 @@
+<template>
+	<view class="money-item flex">
+		<view class="type-icon">
+			<text>囍</text>
+		</view>
+		<view class="info flex">
+			<view class="user-info flex f-ai-c">
+				<text>{{item.name}}</text>
+				<text>{{item.userRemark}}</text>
+			</view>
+			<view class="date">
+				<text>{{item.type}}</text>
+				<text>{{item.time}}</text>
+			</view>
+		</view>
+		<text class="money">-{{item.money}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"songli-list",
+		props: {
+			item: null,
+			index: 0,
+			extraData: null
+		},
+		data() {
+			return {
+				
+			};
+		},
+		onShow() {
+			console.log(343434343, this.extraData);
+		},
+		methods: {
+			titleClick(title){
+				//如果要把点击事件传给页面，可以通过给extraData中添加对应的函数，然后在当前组件中触发这个函数，在页面中监听即可
+				//注意，微信小程序中无法通过props传递事件，在微信小程序中可以使用uni.$emit、uni.$on代替
+				if(this.extraData.titleClickedCallback){
+					this.extraData.titleClickedCallback(title);
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+		.money-item {
+			height: 130rpx;
+			border-bottom: 2rpx solid rgba(180, 182, 186, 0.2);
+			&:last-child {
+				border: none;
+			}
+		}
+		.type-icon {
+			width: 80rpx;
+			height: 80rpx;
+			margin-right: 20rpx;
+			border-radius: 50%;
+			background: rgb(239, 231, 216);
+			text-align: center;
+			line-height: 80rpx;
+			font-weight: 500;
+			color: rgb(222, 98, 91);
+		}
+		.info {
+			flex-grow: 1;
+			flex-direction: column;
+			// justify-content: space-between;
+			.user-info {
+				margin-bottom: 10rpx;
+				text:nth-child(1) {
+					font-weight: 700;
+					font-size: 32rpx;
+				}
+				text:nth-child(2) {
+					display: block;
+					text-align: center;
+					margin-left: 20rpx;
+					padding: 6rpx;
+					border-radius: 4rpx;
+					background-color: rgb(239,214,161);
+					font-weight: 700;
+					font-size: 20rpx;
+					color: rgb(250, 255, 255);
+				}
+			}
+			.date {
+				font-size: 22rpx;
+				color: rgb(70, 70, 70);
+				text:nth-child(1) {
+					position: relative;
+					margin-right: 20rpx;
+					&::after {
+						position: absolute;
+						content: '';
+						top: 50%;
+						right: -12rpx;
+						transform: translateY(-50%);
+						width: 2rpx;
+						height: 80%;
+						background-color: rgb(70, 70, 70, 0.7);
+					}
+				}
+			}
+		}
+		.money {
+			flex-grow: 1;
+			display: block;
+			text-align: right;
+			font-weight: 700;
+		}
+	
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -14,24 +14,19 @@
 				<image class="image" :src="item" mode="aspectFill"></image>
 			</swiper-item>
 		</swiper>
-		<scroll-view scroll-y="true" class="moneys">
-			<view class="money-item flex f-ai-c" v-for="(item, index) in moneys" :key="item.id">
-				<view class="type-icon">
-					<text>囍</text>
-				</view>
-				<view class="info flex">
-					<view class="user-info flex f-ai-c">
-						<text>{{item.name}}</text>
-						<text>{{item.userRemark}}</text>
-					</view>
-					<view class="date">
-						<text>{{item.type}}</text>
-						<text>{{item.time}}</text>
-					</view>
-				</view>
-				<text class="money">-{{item.money}}</text>
-			</view>
-		</scroll-view>
+		<view class="moneys">
+			<z-paging
+				ref="paging" 
+				use-virtual-list 
+				use-compatibility-mode 
+				:fixed="false"
+				:extra-data="extraData" 
+				:cell-height-mode="tabIndex===0?'fixed':'dynamic'" 
+				@query="queryList" 
+				@innerCellClick="innerCellClick"
+			>
+			</z-paging>	
+		</view>
 	</view>
 </template>
 
@@ -43,32 +38,70 @@
 					'https://cdn.zebraui.com/zebra-ui/images/swipe-demo/swipe1.jpg',
 					'https://cdn.zebraui.com/zebra-ui/images/swipe-demo/swipe2.jpg'
 				],
+				extraData: {
+					// 给这个页面的虚拟列表取一个名字，这样在zp-virtual-cell中可以根据这个名字来区分不同页面的cell
+					id: 'songli',
+					// 这边可以附加其他的当前页面需要传给cell的值
+					// 这里示范了通过props传递方法回调来代替cell组件内部emit的场景，如果需要监听点击cell可以直接通过@innerCellClick监听
+					titleClickedCallback: (title) => {
+						// 点击了标题
+						console.log('点击了标题' + title)
+					}
+				},
 				moneys: [
+					/*
 					{ id: 1, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
 					{ id: 2, name: '李四', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
 					{ id: 3, name: '王五', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
 					{ id: 4, name: '赵六', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
-					{ id: 5, name: '张三', userRemark: '亲友备注', money: 19999923, type: '结婚', time: '2023-01-01' },
+					{ id: 5, name: '马齐', userRemark: '亲友备注', money: 19999923, type: '结婚', time: '2023-01-01' },
 					{ id: 6, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
 					{ id: 7, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
 					{ id: 8, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
 					{ id: 9, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
 					{ id: 10, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
 					{ id: 11, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' }
-				]
+					*/
+				],
+				inputVal: ''
+			}
+		},
+		methods: {
+			queryList(pageNo, pageSize) {
+				// 组件加载时会自动触发此方法，因此默认页面加载时会自动触发，无需手动调用
+				// 这里的pageNo和pageSize会自动计算好，直接传给服务器即可
+				// 模拟请求服务器获取分页数据，请替换成自己的网络请求
+				this.moneys = this.moneys.concat([
+					{ id: 1, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
+					{ id: 2, name: '李四', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
+					{ id: 3, name: '王五', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
+					{ id: 4, name: '赵六', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
+					{ id: 5, name: '马齐', userRemark: '亲友备注', money: 19999923, type: '结婚', time: '2023-01-01' },
+					{ id: 6, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
+					{ id: 7, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
+					{ id: 8, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
+					{ id: 9, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
+					{ id: 10, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' },
+					{ id: 11, name: '张三', userRemark: '亲友备注', money: 123, type: '结婚', time: '2023-01-01' }
+				])
+				this.$refs.paging.complete(this.moneys);
+				console.log(this.moneys)
+			},
+			// 内置列表cell被点击时触发，会自动带两个参数：item和index过来
+			innerCellClick(item, index){
+				console.log('点击了' + item.title)
 			}
 		}
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 	.content {
-		padding-bottom: 280rpx;
+		--swiper-height: 260rpx;
 		padding-top: env(safe-area-inset-top);
-		box-sizing: border-box;
 		.swiper {
 			width: 100%;
-			height: 260rpx;
+			height: var(--swiper-height);
 			overflow: hidden;
 			.swiper-item {
 				border-radius: 15rpx;
@@ -80,95 +113,25 @@
 			}
 		}
 		.moneys {
+			// #ifdef H5
+			height: calc(100vh - var(--swiper-height) - 70px);
+			// #endif
+			// #ifdef MP-WEIXIN
+			height: calc(100vh - var(--swiper-height) - 30px);
+			// #endif
 			margin-top: 20rpx;
 			box-sizing: border-box;
 			padding: 25rpx 20rpx;
 			border-radius: 16rpx;
 			box-shadow: 0 0 25rpx rgba(130, 130, 130, 0.2);
-			.money-item {
-				height: 130rpx;
-				border-bottom: 2rpx solid rgba(180, 182, 186, 0.2);
-				&:last-child {
-					border: none;
-				}
-				.type-icon {
-					width: 80rpx;
-					height: 80rpx;
-					margin-right: 20rpx;
-					border-radius: 50%;
-					background: rgb(239, 231, 216);
-					text-align: center;
-					line-height: 80rpx;
-					font-weight: 500;
-					color: rgb(222, 98, 91);
-				}
-				.info {
-					flex-grow: 1;
-					flex-direction: column;
-					justify-content: space-between;
-					.user-info {
-						margin-bottom: 10rpx;
-						text:nth-child(1) {
-							font-weight: 700;
-							font-size: 32rpx;
-						}
-						text:nth-child(2) {
-							display: block;
-							text-align: center;
-							margin-left: 20rpx;
-							padding: 6rpx;
-							border-radius: 4rpx;
-							background-color: rgb(239,214,161);
-							font-weight: 700;
-							font-size: 20rpx;
-							color: rgb(250, 255, 255);
-						}
-					}
-					.date {
-						font-size: 22rpx;
-						color: rgb(70, 70, 70);
-						text:nth-child(1) {
-							position: relative;
-							margin-right: 20rpx;
-							&::after {
-								position: absolute;
-								content: '';
-								top: 50%;
-								right: -12rpx;
-								transform: translateY(-50%);
-								width: 2rpx;
-								height: 80%;
-								background-color: rgb(70, 70, 70, 0.7);
-							}
-						}
-					}
-				}
-				.money {
-					flex-grow: 1;
-					display: block;
-					text-align: right;
-					font-weight: 700;
-				}
-			}
+			scrollbar-width: none;
+			-ms-overflow-style: none;
 		}
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		::-webkit-scrollbar {
+		  display: none;
+		  width: 0;
+		  height: 0;
+		  color: transparent;
+		}
 	}
 </style>
